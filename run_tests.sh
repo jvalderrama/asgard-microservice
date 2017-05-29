23431a53de70d2028023c4e073d613890769c17d
SF initial configurator <admin@sftests.com>
2017-05-29 09:18:42 +0000

    First commit to Asgard Microservice
    Change-Id: Ie44c8e1d32e0518403568897fd91184587273ed1

---
+++ run_tests.sh
@@ -0,0 +1,2 @@
+echo "Launch unit tests"
+python test/unit/hello_world_ut.py
---
+++ src/hello_world.py
@@ -0,0 +1,10 @@
+class hello_world():
+
+	def say_hello(self):
+		hello =  "Hello World!!"
+		print hello
+		return hello
+
+
+if __name__ == '__main__':
+	hello_world().say_hello()
