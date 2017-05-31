7f7e75f10f7ccb1099d24bab5dda37db33d96483
SF initial configurator <admin@sftests.com>
2017-05-31 08:56:47 +0000

    Add functional tests to the project
    Change-Id: I01d99551bc70299863ac06265d08f15f1baa2fc5

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='src',
-      version='1.0',
+      version='1.1',
       description='Source repository',
       author='Asgard',
       author_email='manuel.sanchezlopez@altran.com',
